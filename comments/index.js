917d2cb2fab41d8011aecb1fa99778906237c3cf
MashhoorKhanP <mashhoorkhan325pc@gmail.com>
2024-04-29 15:57:58 +0530

    Done Upto Comment Listing

---
+++ comments/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.status(200).json(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content });
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).json(comments);
+
+});
+
+
+app.listen(4001, () => {
+  console.log("Listening on http://localhost:4001");
+})
